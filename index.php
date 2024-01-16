3571195ef674a7b8039d656d47c245e4d85e82c0
agskanchana <sameerakanchana@live.com>
2024-01-16 12:04:44 +0530

    updated styles

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Bspss PMPPro city filterr
 Plugin URI: https://github.com/agskanchana/bspss-pmpro-city-filter
-Version: 1.0.9
+Version: 1.1.0
 Author: sameera
 Author URI: www.linkedin.com/in/sameera-kanchana-3b4660198
 
